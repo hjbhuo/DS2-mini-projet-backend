40d11f5a64d114bc7899470d52393d2603071762
hayfa <zaghdoudihayfa@gmail.com>
2025-12-17 22:43:16 -0800

    zedna  fel AuthService fonction register  o maah  hash mtaa mot de passe o il rôle TECH par défaut

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User, UserRole } from '../users/user.entity';
+import { RegisterDto } from './dto/create-auth.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(dto: RegisterDto): Promise<User> {
+    const { email, username, password } = dto;
+
+    const existing = await this.usersRepository.findOne({ where: { email } });
+    if (existing) throw new BadRequestException('Email déjà utilisé');
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = this.usersRepository.create({
+      email,
+      username,
+      password: hash,
+      role: UserRole.TECH, // par défaut TECH
+    });
+
+    return this.usersRepository.save(user);
+  }
+
+  
+}
